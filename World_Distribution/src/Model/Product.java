881faef59bb93493e93bd526ea2f28f0c5adee8a
santiagoRosero <39218814+santiagoRosero@users.noreply.github.com>
2020-08-31 19:38:33 -0500

    add initial files
    Add initial files to begin the project

---
+++ World_Distribution/src/Model/Product.java
@@ -0,0 +1,12 @@
+package Model;
+
+public class Product {
+	
+	public final static String TYPE_A="typeA";
+	public final static String TYPE_B="typeB";
+	public final static double VOLUME_A=10;
+	public final static double VOLUME_B=15;
+
+	private String type;
+	private double volume;
+}
